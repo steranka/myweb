8bbb4950398047e945c4fcc5025a1eb652066f57
Patrick Steranka <Patrick@Steranka.com>
2016-10-23 19:21:55 -0700

    Checkpoint

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,40 @@
+// From https://gist.github.com/danharper/3ca2273125f500429945
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
-var babelify = require('babelify');
+var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
 
-var jsxSources = [ "./client/jsx/ClientApp.jsx", "./client/jsx/HelloUI.jsx"];
+function compile(watch) {
+    var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel));
 
-gulp.task('jsx',function(){
-    return browserify({
-		entries: jsxSources,
-	})
-	.transform(babelify)
-	.bundle()
-	.pipe(source('bundle.js'))
-	.pipe(gulp.dest('./server/public/js'));
-});
+    function rebundle() {
+        bundler.bundle()
+            .on('error', function(err) { console.error(err); this.emit('end'); })
+            .pipe(source('build.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./build'));
+    }
 
-gulp.task('dev',['jsx'],function(){
-    gulp.watch('./client/jsx/*.jsx',['jsx'])
-})
+    if (watch) {
+        bundler.on('update', function() {
+            console.log('-> bundling...');
+            rebundle();
+        });
+    }
+
+    rebundle();
+}
+
+function watch() {
+    return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
